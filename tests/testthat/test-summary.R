ngreifer/MatchIt

commit 5622af2600c860081ea0a793f1bb493a6cf33e06

Code cleaning and improvements

R/summary.matchit.R
@@ -268,17 +268,19 @@ summary.matchit <- function(object,
     sum.matched <- do.call("rbind", aa.matched)
     dimnames(sum.matched) <- list(nam, names(aa.matched[[1L]]))
 
-    if (no_x) sum.matched <- sum.matched[-1, , drop = FALSE]
+    if (no_x) sum.matched <- sum.matched[-1L, , drop = FALSE]
     sum.matched.int <- NULL
   }
 
   if (!no_x && interactions) {
     n.int <- kk * (kk + 1) / 2
-    if (un) sum.all.int <- matrix(NA_real_, nrow = n.int, ncol = length(aa.all[[1L]]),
-                                  dimnames = list(NULL, names(aa.all[[1L]])))
+    if (un) {
+      sum.all.int <- make_matrix(names(aa.all[[1L]]), nrow = n.int)
+    }
 
-    if (matched) sum.matched.int <- matrix(NA_real_, nrow = n.int, ncol = length(aa.matched[[1L]]),
-                                           dimnames = list(NULL, names(aa.matched[[1L]])))
+    if (matched) {
+      sum.matched.int <- make_matrix(names(aa.matched[[1L]]), nrow = n.int)
+    }
 
     to.remove <- rep.int(FALSE, n.int)
     int.names <- character(n.int)
@@ -295,18 +297,17 @@ summary.matchit <- function(object,
             sum.all.int[k, ] <- bal1var(x2, tt = treat, ww = NULL, s.weights = s.weights,
                                         standardize = standardize, s.d.denom = s.d.denom)
           }
+
           if (matched) {
             sum.matched.int[k, ] <- bal1var(x2, tt = treat, ww = weights, s.weights = s.weights,
                                             subclass = object$subclass, mm = object$match.matrix,
                                             standardize = standardize, s.d.denom = s.d.denom,
                                             compute.pair.dist = pair.dist)
           }
-          if (i == j) {
-            #Add superscript 2
-            int.names[k] <- paste0(nam[i], "\u00B2")
-          }
-          else {
-            int.names[k] <- paste(nam[i], nam[j], sep = " * ")
+
+          int.names[k] <- {
+            if (i == j) paste0(nam[i], "\u00B2")
+            else paste(nam[i], nam[j], sep = " * ")
           }
         }
         k <- k + 1L
@@ -328,6 +329,7 @@ summary.matchit <- function(object,
     if (un) {
       ad.all <- bal1var(object$distance, tt = treat, ww = NULL, s.weights = s.weights,
                         standardize = standardize, s.d.denom = s.d.denom)
+
       if (exists("sum.all", inherits = FALSE)) {
         sum.all <- rbind(ad.all, sum.all)
         rownames(sum.all)[1L] <- "distance"
@@ -337,10 +339,12 @@ summary.matchit <- function(object,
                           dimnames = list("distance", names(ad.all)))
       }
     }
+
     if (matched) {
       ad.matched <- bal1var(object$distance, tt = treat, ww = weights, s.weights = s.weights,
                             subclass = object$subclass, mm = object$match.matrix, standardize = standardize,
                             s.d.denom = s.d.denom, compute.pair.dist = pair.dist)
+
       if (exists("sum.matched", inherits = FALSE)) {
         sum.matched <- rbind(ad.matched, sum.matched)
         rownames(sum.matched)[1L] <- "distance"
@@ -354,8 +358,8 @@ summary.matchit <- function(object,
 
   ## Imbalance Reduction
   if (matched && un && improvement) {
-    reduction <- matrix(NA_real_, nrow = nrow(sum.all), ncol = ncol(sum.all) - 2L,
-                        dimnames = list(rownames(sum.all), colnames(sum.all)[-(1:2)]))
+    reduction <- make_matrix(colnames(sum.all)[-(1:2)], nrow = rownames(sum.all))
+
     stat.all <- abs(sum.all[, -(1:2), drop = FALSE])
     stat.matched <- abs(sum.matched[, -(1:2), drop = FALSE])
 
@@ -473,10 +477,14 @@ summary.matchit.subclass <- function(object,
 
   if (interactions) {
     n.int <- kk * (kk + 1) / 2
-    if (un) sum.all.int <- matrix(NA_real_, nrow = n.int, ncol = length(aa.all[[1L]]),
-                                  dimnames = list(NULL, names(aa.all[[1L]])))
-    if (matched) sum.matched.int <- matrix(NA_real_, nrow = n.int, ncol = length(aa.matched[[1L]]),
-                                           dimnames = list(NULL, names(aa.matched[[1L]])))
+
+    if (un) {
+      sum.all.int <- make_matrix(names(aa.all[[1L]]), nrow = n.int)
+    }
+
+    if (matched) {
+      sum.matched.int <- make_matrix(names(aa.matched[[1L]]), nrow = n.int)
+    }
 
     to.remove <- rep.int(FALSE, n.int)
     int.names <- character(n.int)
@@ -484,6 +492,7 @@ summary.matchit.subclass <- function(object,
     for (i in seq_len(kk)) {
       for (j in i:kk) {
         x2 <- X[, i] * X[, j]
+
         if (all(abs(x2) < sqrt(.Machine$double.eps)) ||
             all(abs(x2 - X[, i]) < sqrt(.Machine$double.eps))) { #prevent interactions within same factors
           to.remove[k] <- TRUE
@@ -493,18 +502,19 @@ summary.matchit.subclass <- function(object,
             sum.all.int[k, ] <- bal1var(x2, tt = treat, ww = NULL, s.weights = s.weights,
                                         standardize = standardize, s.d.denom = s.d.denom)
           }
+
           if (matched) {
             sum.matched.int[k, ] <- bal1var(x2, tt = treat, ww = weights, s.weights = s.weights,
                                             subclass = subclass, standardize = standardize,
                                             compute.pair.dist = pair.dist)
           }
-          if (i == j) {
-            int.names[k] <- paste0(nam[i], "\u00B2")
-          }
-          else {
-            int.names[k] <- paste(nam[i], nam[j], sep = " * ")
+
+          int.names[k] <- {
+            if (i == j) paste0(nam[i], "\u00B2")
+            else paste(nam[i], nam[j], sep = " * ")
           }
         }
+
         k <- k + 1L
       }
     }
@@ -513,6 +523,7 @@ summary.matchit.subclass <- function(object,
       rownames(sum.all.int) <- int.names
       sum.all <- rbind(sum.all, sum.all.int[!to.remove, , drop = FALSE])
     }
+
     if (matched) {
       rownames(sum.matched.int) <- int.names
       sum.matched <- rbind(sum.matched, sum.matched.int[!to.remove, , drop = FALSE])
@@ -526,6 +537,7 @@ summary.matchit.subclass <- function(object,
       sum.all <- rbind(ad.all, sum.all)
       rownames(sum.all)[1L] <- "distance"
     }
+
     if (matched) {
       ad.matched <- bal1var(object$distance, tt = treat, ww = weights, s.weights = s.weights,
                             subclass = subclass, standardize = standardize,
@@ -557,8 +569,7 @@ summary.matchit.subclass <- function(object,
                          standardize = standardize, which.subclass = s)
       }), colnames(X))
 
-      sum.sub <- matrix(NA_real_, nrow = kk, ncol = ncol(aa[[1L]]),
-                        dimnames = list(nam, colnames(aa[[1L]])))
+      sum.sub <- make_matrix(colnames(aa[[1L]]), nrow = nam)
 
       sum.sub.int <- NULL
       for (i in seq_len(kk)) {
@@ -566,8 +577,8 @@ summary.matchit.subclass <- function(object,
       }
 
       if (interactions) {
-        sum.sub.int <- matrix(NA_real_, nrow = kk * (kk + 1) / 2, ncol = length(aa[[1L]]),
-                              dimnames = list(NULL, names(aa[[1L]])))
+        sum.sub.int <- make_matrix(names(aa[[1L]]), nrow = kk * (kk + 1) / 2)
+
         to.remove <- rep.int(FALSE, nrow(sum.sub.int))
         int.names <- character(nrow(sum.sub.int))
         k <- 1L
@@ -575,20 +586,21 @@ summary.matchit.subclass <- function(object,
           for (j in i:kk) {
             if (!to.remove[k]) { #to.remove defined above
               x2 <- X[, i] * X[, j]
-              jqoi <- bal1var.subclass(x2, tt = treat, s.weights = s.weights,
-                                       subclass = subclass, s.d.denom = s.d.denom,
-                                       standardize = standardize, which.subclass = s)
-              sum.sub.int[k, ] <- jqoi
-              if (i == j) {
-                int.names[k] <- paste0(nam[i], "\u00B2")
-              }
-              else {
-                int.names[k] <- paste(nam[i], nam[j], sep = " * ")
+
+              sum.sub.int[k, ] <- bal1var.subclass(x2, tt = treat, s.weights = s.weights,
+                                                   subclass = subclass, s.d.denom = s.d.denom,
+                                                   standardize = standardize, which.subclass = s)
+
+              int.names[k] <- {
+                if (i == j) paste0(nam[i], "\u00B2")
+                else paste(nam[i], nam[j], sep = " * ")
               }
             }
+
             k <- k + 1L
           }
         }
+
         rownames(sum.sub.int) <- int.names
 
         sum.sub <- rbind(sum.sub, sum.sub.int[!to.remove, , drop = FALSE])

R/aux_functions.R
@@ -40,7 +40,7 @@ create_info <- function(method, fn1, link, discard, replace, ratio,
 #Function to turn a method name into a phrase describing the method
 info_to_method <- function(info) {
 
-  out.list <- setNames(vector("list", 3L), c("kto1", "type", "replace"))
+  out.list <- make_list(c("kto1", "type", "replace"))
 
   if (is_not_null(info$ratio)) {
     out.list[["kto1"]] <- sprintf("%s%s:1",
@@ -403,12 +403,12 @@ matchit_try <- function(expr, from = NULL, dont_warn_if = NULL) {
   warning = function(w) {
     if (is_null(dont_warn_if) || !any(vapply(dont_warn_if, grepl, logical(1L), conditionMessage(w), fixed = TRUE))) {
       if (is_null(from)) arg::wrn("{conditionMessage(w)}")
-      else arg::wrn("(from {from}) {conditionMessage(w)}")
+      else arg::wrn(sprintf("(from %s) {conditionMessage(w)}", from))
     }
     invokeRestart("muffleWarning")
   },
   error = function(e) {
     if (is_null(from)) arg::err("{conditionMessage(e)}")
-    else arg::err("(from {from}) {conditionMessage(e)}")
+    else arg::err(sprintf("(from %s) {conditionMessage(e)}", from))
   })
 }

R/distance2_methods.R
@@ -1,6 +1,5 @@
 #' Propensity scores and other distance measures
 #' @name distance
-#' @aliases distance
 #' @usage NULL
 #'
 #' @description
@@ -308,7 +307,8 @@ distance2glm <- function(formula, data = NULL, link = "logit", ...) {
   linear <- is_not_null(link) && startsWith(as.character(link), "linear")
   if (linear) link <- sub("linear.", "", as.character(link), fixed = TRUE)
 
-  args <- unique(c(names(formals(glm)), names(formals(glm.control))))
+  args <- unique(c(rlang::fn_fmls_names(glm),
+                   rlang::fn_fmls_names(glm.control)))
   A <- ...mget(args)
   A[lengths(A) == 0L] <- NULL
 
@@ -319,7 +319,9 @@ distance2glm <- function(formula, data = NULL, link = "logit", ...) {
     else quasibinomial(link = link)
   }
 
-  res <- do.call("glm", A)
+  res <- matchit_try({
+    do.call("glm", A)
+  }, from = "{.fun stats::glm}")
 
   pred <- predict(res, type = if (linear) "link" else "response")
 
@@ -337,9 +339,11 @@ distance2gam <- function(formula, data = NULL, link = "logit", ...) {
   weights <- A[["weights"]]
   A[["weights"]] <- NULL
 
-  res <- do.call(mgcv::gam, c(list(formula, data, family = quasibinomial(link),
-                                   weights = weights), A),
-                 quote = TRUE)
+  res <- matchit_try({
+    do.call(mgcv::gam, c(list(formula, data, family = quasibinomial(link),
+                              weights = weights), A),
+            quote = TRUE)
+  }, from = "{.fun mgcv::gam}")
 
   pred <- predict(res, type = if (linear) "link" else "response")
 
@@ -350,7 +354,8 @@ distance2gam <- function(formula, data = NULL, link = "logit", ...) {
 distance2rpart <- function(formula, data = NULL, link = NULL, ...) {
   rlang::check_installed("rpart")
 
-  args <- unique(c(names(formals(rpart::rpart)), names(formals(rpart::rpart.control))))
+  args <- unique(c(rlang::fn_fmls_names(rpart::rpart),
+                   rlang::fn_fmls_names(rpart::rpart.control)))
   A <- ...mget(args)
   A[lengths(A) == 0L] <- NULL
 
@@ -358,7 +363,10 @@ distance2rpart <- function(formula, data = NULL, link = NULL, ...) {
   A[["data"]] <- data
   A[["method"]] <- "class"
 
-  res <- do.call(rpart::rpart, A)
+  res <- matchit_try({
+    do.call(rpart::rpart, A)
+  }, from = "{.fun rpart::rpart}")
+
   list(model = res, distance = predict(res, type = "prob")[, "1"])
 }
 
@@ -370,9 +378,11 @@ distance2nnet <- function(formula, data = NULL, link = NULL, ...) {
   weights <- A[["weights"]]
   A[["weights"]] <- NULL
 
-  res <- do.call(nnet::nnet, c(list(formula, data = data,
-                                    weights = weights, entropy = TRUE), A),
-                 quote = TRUE)
+  res <- matchit_try({
+    do.call(nnet::nnet, c(list(formula, data = data,
+                               weights = weights, entropy = TRUE), A),
+            quote = TRUE)
+  }, from = "{.fun nnet::nnet}")
 
   list(model = res, distance = drop(fitted(res)))
 }
@@ -408,9 +418,11 @@ distance2cbps <- function(formula, data = NULL, link = NULL, ...) {
   A[["formula"]] <- formula
   A[["data"]] <- data
 
-  capture.output({ #Keeps from printing message about treatment
-    res <- do.call(CBPS::CBPS, A, quote = TRUE)
-  })
+  matchit_try({
+    capture.output({ #Keeps from printing message about treatment
+      res <- do.call(CBPS::CBPS, A, quote = TRUE)
+    })
+  }, from = "{.fun CBPS::CBPS}")
 
   pred <- fitted(res)
   if (linear) pred <- qlogis(pred)
@@ -424,14 +436,17 @@ distance2bart <- function(formula, data = NULL, link = NULL, ...) {
 
   linear <- is_not_null(link) && startsWith(as.character(link), "linear")
 
-  args <- unique(c(names(formals(dbarts::bart2)), names(formals(dbarts::dbartsControl))))
+  args <- unique(c(rlang::fn_fmls_names(dbarts::bart2),
+                   rlang::fn_fmls_names(dbarts::dbartsControl)))
   A <- ...mget(args)
   A[lengths(A) == 0L] <- NULL
 
   A[["formula"]] <- formula
   A[["data"]] <- data
 
-  res <- do.call(dbarts::bart2, A)
+  res <- matchit_try({
+    do.call(dbarts::bart2, A)
+  }, from = "{.fun dbarts::bart2}")
 
   pred <- fitted(res, type = if (linear) "link" else "response")
 
@@ -471,7 +486,7 @@ distance2bart <- function(formula, data = NULL, link = NULL, ...) {
 #   res <- do.call(fun, c(list(X,
 #                              y.train = treat,
 #                              type = switch(link, "logit" = "lbart", "pbart")),
-#                         A[intersect(names(A), setdiff(names(formals(fun)),
+#                         A[intersect(names(A), setdiff(rlang::fn_fmls_names(fun),
 #                                                       c("x.train", "y.train", "x.test", "type", "ntype")))]))
 #
 #   pred <- res$prob.train.mean
@@ -483,10 +498,14 @@ distance2bart <- function(formula, data = NULL, link = NULL, ...) {
 #distance2randomforest-----------------
 distance2randomforest <- function(formula, data = NULL, link = NULL, ...) {
   rlang::check_installed("randomForest")
+
   newdata <- get_all_vars(formula, data)
   treatvar <- as.character(formula[[2L]])
   newdata[[treatvar]] <- factor(newdata[[treatvar]], levels = c("0", "1"))
-  res <- randomForest::randomForest(formula, data = newdata, ...)
+
+  res <- matchit_try({
+    randomForest::randomForest(formula, data = newdata, ...)
+  }, from = "{.fun randomForest::randomForest}")
 
   list(model = res, distance = predict(res, type = "prob")[, "1"])
 }
@@ -500,7 +519,8 @@ distance2elasticnet <- function(formula, data = NULL, link = NULL, ...) {
 
   s <- ...get("s") %or% "lambda.1se"
 
-  args <- unique(c(names(formals(glmnet::glmnet)), names(formals(glmnet::cv.glmnet))))
+  args <- unique(c(rlang::fn_fmls_names(glmnet::glmnet),
+                   rlang::fn_fmls_names(glmnet::cv.glmnet)))
   A <- ...mget(args)
   A[lengths(A) == 0L] <- NULL
 
@@ -518,16 +538,21 @@ distance2elasticnet <- function(formula, data = NULL, link = NULL, ...) {
   A$y <- model.response(mf)
   A$x <- model.matrix(update(formula, . ~ . + 1), mf)[, -1L, drop = FALSE]
 
-  res <- do.call(glmnet::cv.glmnet, A)
+  res <- matchit_try({
+    do.call(glmnet::cv.glmnet, A)
+  }, from = "{.fun glmnet::cv.glmnet}")
 
-  pred <- drop(predict(res, newx = A$x, s = s,
-                       type = if (linear) "link" else "response"))
+  pred <- matchit_try({
+    drop(predict(res, newx = A$x, s = s,
+                 type = if (linear) "link" else "response"))
+  }, from = "{.fun glmnet::predict.glmnet}")
 
   list(model = res, distance = pred)
 }
 distance2lasso <- function(formula, data = NULL, link = NULL, ...) {
   if ("alpha" %in% ...names()) {
-    args <- unique(c("s", names(formals(glmnet::glmnet)), names(formals(glmnet::cv.glmnet))))
+    args <- unique(c("s", rlang::fn_fmls_names(glmnet::glmnet),
+                     rlang::fn_fmls_names(glmnet::cv.glmnet)))
     A <- ...mget(args)
     A[lengths(A) == 0L] <- NULL
 
@@ -541,7 +566,8 @@ distance2lasso <- function(formula, data = NULL, link = NULL, ...) {
 }
 distance2ridge <- function(formula, data = NULL, link = NULL, ...) {
   if ("alpha" %in% ...names()) {
-    args <- unique(c("s", names(formals(glmnet::glmnet)), names(formals(glmnet::cv.glmnet))))
+    args <- unique(c("s", rlang::fn_fmls_names(glmnet::glmnet),
+                     rlang::fn_fmls_names(glmnet::cv.glmnet)))
     A <- ...mget(args)
     A[lengths(A) == 0L] <- NULL
 
@@ -562,7 +588,7 @@ distance2gbm <- function(formula, data = NULL, link = NULL, ...) {
 
   method <- ...get("method")
 
-  args <- names(formals(gbm::gbm))
+  args <- rlang::fn_fmls_names(gbm::gbm)
   A <- ...mget(args)
   A[lengths(A) == 0L] <- NULL
 
@@ -589,9 +615,13 @@ distance2gbm <- function(formula, data = NULL, link = NULL, ...) {
     arg::err("{.arg distance.options$method} should be one of {.val OOB} or {.val cv}")
   }
 
-  res <- do.call(gbm::gbm, A)
+  res <- matchit_try({
+    do.call(gbm::gbm, A)
+  }, from = "{.fun gbm::gbm}")
 
-  best.tree <- gbm::gbm.perf(res, plot.it = FALSE, method = method)
+  best.tree <- matchit_try({
+    gbm::gbm.perf(res, plot.it = FALSE, method = method)
+  }, from = "{.fun gbm::gbm.perf}")
 
   pred <- drop(predict(res, newdata = data, n.trees = best.tree,
                        type = if (linear) "link" else "response"))

R/input_processing.R
@@ -137,12 +137,12 @@ check.inputs <- function(mcall, method, distance, link, distance.options, exact,
   method_str <- if (null.method) "NULL" else dQuote(method, FALSE)
 
   if (is_not_null(ignored.inputs)) {
-    arg::wrn("the argument{?s} {.arg {ignored.inputs}} {?is/are} not used with {.code method = {method_str}} and will be ignored")
+    arg::wrn("the {cli::qty(ignored.inputs)} argument{?s} {.arg {ignored.inputs}} {?is/are} not used with {.code method = {method_str}} and will be ignored")
   }
 
   if (is_not_null(error.inputs)) {
     distance_str <- dQuote(distance, FALSE)
-    arg::err("the argument{?s} {.arg {error.inputs}} cannot be used with {.code method = {method_str}} and {.code distance = {distance_str}}")
+    arg::err("the {cli::qty(error.inputs)} argument{?s} {.arg {error.inputs}} cannot be used with {.code method = {method_str}} and {.code distance = {distance_str}}")
   }
 
   ignored.inputs

R/match.qoi.R
@@ -79,8 +79,7 @@ bal1var.subclass <- function(xx, tt, s.weights, subclass, s.d.denom = "treated",
   bin.var <- all(xx == 0 | xx == 1)
   in.sub <- !is.na(subclass) & subclass == which.subclass
 
-  xsum <- matrix(NA_real_, nrow = 1L, ncol = 6L)
-  rownames(xsum) <- "Subclass"
+  xsum <- make_matrix(ncol = 6L, nrow = "Subclass")
   colnames(xsum) <- {
     if (standardize) c("Means Treated", "Means Control", "Std. Mean Diff.",
                        "Var. Ratio", "eCDF Mean", "eCDF Max")

R/match_data.R
@@ -1,6 +1,5 @@
 #' Construct a matched dataset from a `matchit` object
 #' @name match_data
-#' @aliases match_data match.data get_matches
 #'
 #' @description
 #' `match_data()` and `get_matches()` create a data frame with
@@ -319,8 +318,9 @@ get_matches <- function(object,
 
   num.matches <- rowSums(!is.na(mm))
 
-  matched <- as.data.frame(matrix(NA_character_, nrow = nrow(mm) + sum(!is.na(mm)), ncol = 3L))
-  names(matched) <- c(id, subclass, weights)
+  matched <- make_df(c(id, subclass, weights),
+                     nrow = nrow(mm) + sum(!is.na(mm)),
+                     types = "character")
 
   matched[[id]] <- c(as.vector(tmm[!is.na(tmm)]),
                      rownames(mm))

R/matchit2cem.R
@@ -537,8 +537,7 @@ do_k2k <- function(treat, X, subclass, k2k.method = "mahalanobis", mpower = 2, s
                              antiexactcovs, unit.id, m.order, verbose)
   }
   else {
-    mm <- matrix(NA_integer_,  ncol = 1L, nrow = sum(treat == 1),
-                 dimnames = list(names(treat)[treat == 1], NULL))
+    mm <- make_matrix(1L, nrow = names(treat)[treat == 1], type = "integer")
 
     for (s in levels(subclass)) {
       .e <- which(subclass == s)

R/matchit2full.R
@@ -351,7 +351,7 @@ matchit2full <- function(treat, formula, data, distance, discarded,
 
   #Initialize pair membership; must include names
   pair <- rep_with(NA_character_, treat)
-  p <- setNames(vector("list", nlevels(ex)), levels(ex))
+  p <- make_list(levels(ex))
 
   A$data <- data.frame(treat) #just to get rownames; not actually used in matching
   A$min.controls <- min.controls

commit 753dcd0e0b76e94a1638bbeaccc51cb9a3ad11d2

Fix `summary()` with both `subclass = TRUE` and `interactions = TRUE`
The subclass interactions matrix was built from `names()` of the 1x6 matrix
that `bal1var.subclass()` returns, which is `NULL`, so it had no columns and
the `rbind()` failed on every subclassification fit (a regression from
5622af26, in 4.8.0). It now uses `colnames()`.

The subclass block also redefined `to.remove` as all `FALSE`, hiding the
vector computed for the aggregate table that its comment said it reused
(since 9dd3d2fd). It now reuses that vector along with `n.int` and
`int.names`, so each subclass reports the same interactions as the
aggregate, without identically zero products of one factor's dummies or
squares of dummies.

Co-Authored-By: Claude Opus 5.5 <noreply@anthropic.com>

R/summary.matchit.R
@@ -591,24 +591,20 @@ summary.matchit.subclass <- function(object,
       }
 
       if (interactions) {
-        sum.sub.int <- make_matrix(names(aa[[1L]]), nrow = kk * (kk + 1) / 2)
+        #`n.int`, `to.remove`, and `int.names` come from the aggregate balance
+        #statistics above, which run over the same pairs in the same order, so each
+        #subclass reports the same interactions as the aggregate does
+        sum.sub.int <- make_matrix(colnames(aa[[1L]]), nrow = n.int)
 
-        to.remove <- rep.int(FALSE, nrow(sum.sub.int))
-        int.names <- character(nrow(sum.sub.int))
         k <- 1L
         for (i in seq_len(kk)) {
           for (j in i:kk) {
-            if (!to.remove[k]) { #to.remove defined above
+            if (!to.remove[k]) {
               x2 <- X[, i] * X[, j]
 
               sum.sub.int[k, ] <- bal1var.subclass(x2, tt = treat, s.weights = s.weights,
                                                    subclass = subclass, s.d.denom = s.d.denom,
                                                    standardize = standardize, which.subclass = s)
-
-              int.names[k] <- {
-                if (i == j) paste0(nam[i], "\u00B2")
-                else paste(nam[i], nam[j], sep = " * ")
-              }
             }
 
             k <- k + 1L

tests/testthat/test-summary.R
@@ -279,6 +279,31 @@ test_that("summary: the subclass argument selects which subclasses to report", {
   expect_length(summary(m, subclass = c(1, 3))$sum.subclass, 2L)
 })
 
+test_that("summary: subclass balance with interactions", {
+  m <- matchit(f_sum, data = lalonde, method = "subclass", subclass = 4)
+
+  s0 <- summary(m, subclass = TRUE)
+  s <- summary(m, subclass = TRUE, interactions = TRUE)
+
+  expect_named(s$sum.subclass, names(s0$sum.subclass))
+
+  for (sub in names(s$sum.subclass)) {
+    ss <- s$sum.subclass[[sub]]
+    ss0 <- s0$sum.subclass[[sub]]
+
+    expect_true(all(c("age²", "age * educ") %in% rownames(ss)))
+    expect_identical(colnames(ss), colnames(ss0))
+
+    #The main effects are unchanged by adding interactions
+    expect_equal(ss[rownames(ss0), ], ss0)
+
+    #The interactions are the ones reported in aggregate, which omit products that
+    #are identically zero or that repeat a dummy variable
+    expect_identical(rownames(ss), rownames(s$sum.across))
+    expect_false(any(c("married²", "raceblack * racehispan") %in% rownames(ss)))
+  }
+})
+
 test_that("summary: eCDF and eQQ statistics in a subclass where a covariate is 0/1 only", {
   #`x` is not binary overall, but within subclass 1 it takes only 0 and 1, so that
   #subclass reaches the binary shortcut in `qqsum()`. The distance places units with